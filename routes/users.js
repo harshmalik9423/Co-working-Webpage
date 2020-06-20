e8e263543092ad038600b1e6bed6a6e5459bc0f6
Harsh Malik <harshmalik2013@gmail.com>
2020-06-20 13:02:30 +0530

    Setting up middleware for POST and putting restrictions on form data

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,34 @@ const router = express.Router();
 router.get('/login', (req, res) => res.render('login'));              //Login
 router.get('/register', (req, res) => res.render('register'));        //Reigister
 
+// Register POST 
+router.post('/register', (req,res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: "Incomplete Feilds!"});
+    }
+
+    if(password != password2) {
+        errors.push({ msg: 'Passwords does not match'});
+    }
+
+    if(password.length < 6) {
+        errors.push({ msg: 'Password length smaller than 6!'});
+    }
+
+    if(errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('pass');
+    }
+});
+
 module.exports = router;
